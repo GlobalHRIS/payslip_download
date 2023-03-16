f8ef0e10c9625bd8bde5dd994ccb0ae2188abbcb
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-03-16 18:22:58 +0000

    Update functions.py

--- functions.py
+++ functions.py
@@ -104,3 +104,4 @@ def displayPDF(file):
   pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
   # Displaying File
   st.markdown(pdf_display, unsafe_allow_html=True)
+
