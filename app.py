cb24bb264e65e95fd736a78baf9ece2a6c0ac08a
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-03-16 17:05:16 +0000

    Update app.py

--- app.py
+++ app.py
@@ -35,9 +35,7 @@ languages = {
 
 with st.sidebar:
     st.title(":outbox_tray: PDF to Text")
-    textOutput = st.selectbox(
-        "How do you want your output text?",
-        ('Text file (.txt)', 'CSV file(.csv)')
+    textOutput = st.selectbox("How do you want your output text?",('Text file (.txt)', 'CSV file(.csv)')
     ocr_box = st.checkbox('Enable OCR (scanned document)')
 	    
     st.markdown(html_temp.format("rgba(55, 53, 47, 0.16)"),unsafe_allow_html=True)
