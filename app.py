c214f7631bcd57d3205ed4ce007049d15be422e2
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-03-16 16:01:40 +0000

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ with st.sidebar:
     textOutput = st.selectbox(
         "How do you want your output text?",
         ('Text file (.txt)', 'CSV file(.csv)')
-    ocr_box = st.checkbox('Enable OCR (scanned document)')
+	    ocr_box = st.checkbox('Enable OCR (scanned document)')
     
     st.markdown(html_temp.format("rgba(55, 53, 47, 0.16)"),unsafe_allow_html=True)
     st.markdown("""
