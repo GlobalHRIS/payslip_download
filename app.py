0462e342bc8b752e6c6c1425ef963c79e8ff1591
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-03-16 22:42:51 +0000

    Update app.py

--- app.py
+++ app.py
@@ -102,5 +102,21 @@ if pdf_file:
                 mime="application/zip"
 	    )
 	
+st.title('Text to CSV Converter')
+file = st.file_uploader("Upload a text file", type=["txt"])
 
+if file:
+    st.write(f"Uploading file: {file.name}")
+    #text_to_csv(file)
+    data = []
+    for line in file:
+        data.append(line.strip().split())
+        df = pd.DataFrame(data)
+        df.to_csv('output.csv', index=False)
+    st.download_button(
+        label="Download CSV",
+        data=open('output.csv', 'rb').read(),
+        file_name='output.csv',
+        mime='text/csv'
+    )
     
